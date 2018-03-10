995ffe7f193b03d58d588b638be080e6ed7d84de
FFugi <fudifil@gmail.com>
2018-03-10 19:03:08 +0100

    bst destructor

--- src/data-structures/binary-search-tree/cpp/bst.hpp
+++ src/data-structures/binary-search-tree/cpp/bst.hpp
@@ -69,19 +69,11 @@ class Bst{
 		}
 
 		T getMin(){
-			Node * iterator = root;
-			while(iterator->lesser != nullptr){
-				iterator = iterator->lesser;
-			}
-			return iterator->value;
+			return getMinOfSubtree(root)->value;
 		}
 
 		T getMax(){
-			Node * iterator = root;
-			while(iterator->greater != nullptr){
-				iterator = iterator->greater;
-			}
-			return iterator->value;
+			return getMaxOfSubtree(root)->value;
 		}
 
 		std::size_t getNodeCount(){
@@ -188,49 +180,57 @@ class Bst{
 		}
 
 		void deleteValue(T value){
-
+			
 		}
 
 		~Bst(){
+			std::stack<Node*> toDelete;
+			Node * current = root;
+			Node * greaterFromDeleted;
+			toDelete.push(current);
+			bool goingLeft = true;
+			std::size_t deleted = 0;
+			while(deleted != nodeCount){	
+				if(goingLeft){
+					toDelete.push(current);
+				}
+				if(current->lesser != nullptr && goingLeft){
+					current = current->lesser;
+				}
+				else{
+					greaterFromDeleted = current->greater;
+					delete current;
+					deleted++;
+					toDelete.pop();
+					if(greaterFromDeleted != nullptr){
+						current = greaterFromDeleted;
+						goingLeft = true;
+					}
+					else{
+						current = toDelete.top();
+						goingLeft = false;
+					}
+				}
+			}
+		}
 
+	private:
+		Node * getMinOfSubtree(Node * subRoot){
+			Node * iterator = subRoot;
+			while(iterator->lesser != nullptr){
+				iterator = iterator->lesser;
+			}
+			return iterator;
+		}
 
+		Node * getMaxOfSubtree(Node * subRoot){
+			Node * iterator = subRoot;
+			while(iterator->greater != nullptr){
+				iterator = iterator->greater;
+			}
+			return iterator;
 		}
 };
 
 
 #endif
-//void printValues(){
-//	if(nodeCount == 0){
-//		return;
-//	}
-//	else if(nodeCount == 1){
-//		std::cout << root->value << std::endl;
-//		return;
-//	}
-//	std::stack<Node*> toPrint;
-//	Node * current = root;
-//	toPrint.push(current);
-//	bool goingLeft = true;
-//	std::size_t printed = 0;
-//	while(printed != nodeCount){	
-//		if(goingLeft){
-//			toPrint.push(current);
-//		}
-//		if(current->lesser != nullptr && goingLeft){
-//			current = current->lesser;
-//		}
-//		else{
-//			std::cout << current->value << std::endl;
-//			printed++;
-//			toPrint.pop();
-//			if(current->greater != nullptr){
-//				current = current->greater;
-//				goingLeft = true;
-//			}
-//			else{
-//				current = toPrint.top();
-//				goingLeft = false;
-//			}
-//		}
-//	}
-//}
